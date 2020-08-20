c64945bc6912e46dc2dc58c607ca63d577ca8105
lzc <lzcnice666@163.com>
2020-08-20 13:55:12 +0800

    添加注册中心

---
+++ lan-eureka/src/test/java/com/cloud/lan/laneureka/LanEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.lan.laneureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
